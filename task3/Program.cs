6893ac5d2a4d49ac5c1be6c9de277317497b3a78
login4 <birukovartemij@gmail.com>
2023-02-01 11:47:15 +0300

    task4 completed

---
+++ task3/Program.cs
@@ -0,0 +1,66 @@
+﻿void PrintArray(int[,] matr)
+{
+    Console.WriteLine();
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMultMatr(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i<arrayA.GetLength(0); i++)
+    {
+        for (int j=0; j<arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k<arrayA.GetLength(1); k++)
+            {
+                arrayC[i,j] += arrayA[i,k] * arrayB[k,j];
+            }
+        }
+    }
+    return arrayC;
+}
+
+// void MatrPow(int[,] firstMatr, int[,] secondMatr)
+// {
+//     int[,] resultMatr = new int[firstMatr.GetLength(0), secondMatr.GetLength(1)];
+//     for (int rows = 0; rows < firstMatr.GetLength(0); rows++)
+//     {
+//         for (int columns = 0; columns < secondMatr.GetLength(1); columns++)
+//         {
+//             for (int k = 0; k < firstMatr.GetLength(1); k++)
+//             {
+//                 resultMatr[rows, columns] += firstMatr[rows, k] * secondMatr[k, columns];
+//                 Console.Write($"{resultMatr[rows,columns]} ");
+//             }
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+int[,] arr1 = new int[3, 4];
+int[,] arr2 = new int[3, 4];
+
+FillArray(arr1);
+FillArray(arr2);
+PrintArray(arr1);
+PrintArray(arr2);
+
+PrintArray(GetMultMatr(arr1,arr2));
---
+++ task4/Program.cs
@@ -0,0 +1,37 @@
+﻿int[,,] tripleArr = new int[2, 2, 2];
+FillArray(tripleArr);
+PrintIndex(tripleArr);
+
+
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
